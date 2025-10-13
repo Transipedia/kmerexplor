becefc28c660a57807ad1b27a242ca78e8fd0107
benoit <benoit.guibert@inserm.fr>
2025-10-13 14:40:28 +0200

    add Yaxis name and update echarts.js to version 5.6

--- kmerexplor/common.py
+++ kmerexplor/common.py
@@ -4,25 +4,6 @@ import os
 import sys
 import shutil
 
-# ~ APPPATH = os.path.dirname(os.path.realpath(__file__))
-# ~ DEFAULT_TAGSET = "human-quality"
-
-
-# ~ def get_tags_file(args):
-    # ~ """ Function doc """
-    # ~ ### Find tags file
-    # ~ ## --tags used to specify tags file
-    # ~ if args.tags:
-        # ~ tags_file = args.tags
-    # ~ ## tags file specified from --builtin-tags option (default: human)
-    # ~ else:
-        # ~ tags_file = BUILTIN_TAGS[args.builtin_tags]
-    # ~ ### Is tag file here ?
-    # ~ if not os.path.isfile(tags_file):
-        # ~ print("\n FileError: tags file '{}' not Found.\n".format(tags_file))
-        # ~ sys.exit(exit_gracefully(args))
-    # ~ return tags_file
-
 
 def exit_gracefully(args, files_type=None):
     """ Function doc """
